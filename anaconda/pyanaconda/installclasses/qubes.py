62252c5a3c262b2dbc149dd709b24d63178136b9
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-02-14 00:33:56 +0100

    anaconda: replace installclass with Qubes one
    Mainly to modify default partitioning layout.

---
+++ anaconda/pyanaconda/installclasses/qubes.py
@@ -0,0 +1,85 @@
+#
+# qubes.py
+#
+# Copyright (C) 2011  Invisible Things Lab All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from pyanaconda.installclass import BaseInstallClass
+from pyanaconda.constants import *
+from pyanaconda.product import *
+from pyanaconda import network
+from pyanaconda import isys
+
+import os, types
+import gettext
+_ = lambda x: gettext.ldgettext("anaconda", x)
+
+from decimal import Decimal
+
+class InstallClass(BaseInstallClass):
+    # name has underscore used for mnemonics, strip if you dont need it
+    id = "qubes"
+    name = N_("Qubes")
+    _description = N_("The default installation of %s is a minimal install. "
+                      "You can optionally select a different set of software "
+                      "now.")
+    _descriptionFields = (productName,)
+    sortPriority = 20000
+    hidden = 0
+
+    bootloaderTimeoutDefault = 5
+
+    tasks = [(N_("Minimal"), ["base", "base-x", "kde-desktop-qubes", "qubes" ]) ]
+
+    def getPackagePaths(self, uri):
+        if not type(uri) == types.ListType:
+            uri = [uri,]
+
+        return {'Installation Repo': uri}
+
+    def configure(self, anaconda):
+        BaseInstallClass.configure(self, anaconda)
+
+    def setDefaultPartitioning(self, storage):
+        BaseInstallClass.setDefaultPartitioning(self,
+                                                storage)
+        for autoreq in storage.autoPartitionRequests:
+	    if autoreq.mountpoint == "/":
+		autoreq.maxSize=None
+		autoreq.requiredSpace=50*1024
+            if autoreq.mountpoint == "/home":
+		storage.autoPartitionRequests.remove(autoreq)
+
+    def postAction(self, anaconda):
+        # Import rpm keys, so that qubes-receive-updates can call rpm -K
+        subprocess.check_call(['/usr/sbin/chroot', anaconda.rootPath,
+            '/bin/bash', '-c', 'rpm --import /etc/pki/rpm-gpg/*'])
+
+    def productMatches(self, oldprod):
+        if oldprod is None:
+            return False
+
+        if oldprod.startswith(productName):
+            return True
+
+        return False
+
+    def versionMatches(self, oldver):
+        return True
+
+    def __init__(self):
+        BaseInstallClass.__init__(self)
