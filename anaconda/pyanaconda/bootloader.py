5796c78979af338b1173d4901a0069b84f5ecf49
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-07-15 14:28:26 +0200

    anaconda: disable os prober
    It tries to mount every existing block device, including VM images.

--- anaconda/pyanaconda/bootloader.py
+++ anaconda/pyanaconda/bootloader.py
@@ -1457,6 +1457,7 @@ class GRUB2(GRUB):
         defaults.write("GRUB_CMDLINE_XEN_DEFAULT=\"console=none dom0_mem=min:1024M\"\n")
         defaults.write("GRUB_DISABLE_RECOVERY=\"true\"\n")
         defaults.write("GRUB_THEME=\"/boot/grub2/themes/system/theme.txt\"\n")
+        defaults.write("GRUB_DISABLE_OS_PROBER=\"true\"\n")
         defaults.close()
 
     def _encrypt_password(self):
