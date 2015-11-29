aa8c80cbcafb135e8dce28b6a7baf9e021bb4a61
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-11-29 04:16:09 +0100

    firstboot: start sys-usb
    QubesOS/qubes-issues#704

--- firstboot/modules/qubes_setup.py
+++ firstboot/modules/qubes_setup.py
@@ -159,6 +159,10 @@ class moduleClass(Module):
             self.configure_default_template()
             self.configure_qubes()
             self.configure_network()
+            if self.choice_usb.get_selected():
+                # Workaround for #1464 (so qvm.start from salt can't be used)
+                self.run_command_in_thread(['systemctl', 'start',
+                                            'qubes-vm@sys-usb.service'])
 
             try:
                 self.configure_default_dvm()
