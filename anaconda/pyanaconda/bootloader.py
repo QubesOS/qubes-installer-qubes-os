c834daa1765f083937d49495bbb38a591f3dd8c1
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-09-21 22:10:04 +0200

    anaconda: make sure the latest version is placed as xen.efi
    os.listdir returns files in filesystem order, not sorted.
    
    QubesOS/qubes-issues#2990

--- anaconda/pyanaconda/bootloader.py
+++ anaconda/pyanaconda/bootloader.py
@@ -1857,10 +1857,10 @@ class XenEFI(EFIGRUB):
         if os.path.exists(xen_efi_target):
             os.remove(xen_efi_target)
 
-        xen_efi = [x for x in os.listdir(iutil.getSysroot() + self.config_dir) if
-                   x.startswith('xen-') and x.endswith('.efi')][0]
+        xen_efi = [x for x in sorted(os.listdir(iutil.getSysroot() + self.config_dir)) if
+                   x.startswith('xen-') and x.endswith('.efi')][-1]
         shutil.copy("{}/{}".format(iutil.getSysroot() + self.config_dir, xen_efi),
-                    "{}/{}".format(iutil.getSysroot() + self.config_dir, "xen.efi"))
+                    xen_efi_target)
         rc = self.efibootmgr("-c", "-w", "-L", productName,
                              "-d", boot_disk.path, "-p", boot_part_num,
                              "-l",
