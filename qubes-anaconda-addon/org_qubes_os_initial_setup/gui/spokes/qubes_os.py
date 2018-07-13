ea3f48422b6c9bb4873e6ab2dee82b1709298ce5
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-07-13 17:28:46 +0200

    qubes-anaconda-addon, conf: update to Fedora 28 template
    QubesOS/qubes-issues#4013
    
    (cherry picked from commit 2c79b96c406c6296d2a509c8218b10627f4810c0)

--- qubes-anaconda-addon/org_qubes_os_initial_setup/gui/spokes/qubes_os.py
+++ qubes-anaconda-addon/org_qubes_os_initial_setup/gui/spokes/qubes_os.py
@@ -227,7 +227,7 @@ class QubesOsSpoke(FirstbootOnlySpokeMixIn, NormalSpoke):
 
         self.qubes_user = None
         self.qubes_gid = None
-        self.default_template = 'fedora-26'
+        self.default_template = 'fedora-28'
 
         self.set_stage("Start-up")
         self.done = False
