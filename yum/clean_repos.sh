967c9caa7cb60e25290be41895953e004f1d7719
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-01-09 01:34:18 +0100

    Prefer createrepo_c over createrepo

--- yum/clean_repos.sh
+++ yum/clean_repos.sh
@@ -1,8 +1,9 @@
+createrepo="$(which createrepo_c createrepo | head -n 1)"
 for repo in dom0-updates installer qubes-dom0 ; do
     echo "---> Cleaning up repo: $repo..."
     rm -f $repo/rpm/*.rpm
     rm -f $repo/repodata/*
-    createrepo -q $repo
+    $createrepo -q $repo
 done
 
 
